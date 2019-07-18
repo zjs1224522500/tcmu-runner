c41b61ce6fe59e1b93aa20532aeb85d47186ab7c
Elvis <1224522500@qq.com>
2019-07-18 10:10:26 +0800

    Add fragment config

--- handler_hikvision.c
+++ handler_hikvision.c
@@ -40,19 +40,16 @@
 struct hikvision_state {
 	char *iqn;
 	int fd;
-	char *fragment_size;
+	double fragment_size;
+    char *fragment_size_unit;
 };
 
 static int hikvision_open(struct tcmu_device *dev, bool reopen)
 {
 	struct hikvision_state *state;
-	char *cfgString, *first_split_symbol, *second_split_symbol, *file_desc_path, *iqn;
-	int length;
+	char *cfgString, *first_virgule_symbol, *second_virgule_symbol, *semicolon_symbol, *file_desc_path, *iqn, *fragment_size_str;
+	int length, file_path_length, fragment_size_length;
 	struct hikvision_state *hm_private;
-
-	tcmu_err("open tcmu-device with fd %d\n", tcmu_dev_get_fd(dev));
-	tcmu_err("open tcmu-device with fd %s\n", tcmu_dev_get_cfgstring(dev));
-
 
 	state = calloc(1, sizeof(*state));
 	if (!state) 
@@ -63,23 +60,29 @@ static int hikvision_open(struct tcmu_device *dev, bool reopen)
 
 	// Parse the config string to iqn and file desc.
 	cfgString = tcmu_dev_get_cfgstring(dev);
-	first_split_symbol = strchr(cfgString, '/');
-	file_desc_path = first_split_symbol + 1;
+	first_virgule_symbol = strchr(cfgString, '/');
+	semicolon_symbol = strchr(cfgString, ';');
+	fragment_size_str = semicolon_symbol + 1;
+	fragment_size_length = strlen(fragment_size_str);
+	file_path_length = semicolon_symbol - first_virgule_symbol - 1;
+	file_desc_path = (char *) calloc(file_path_length, sizeof(char));
+	strncpy(file_desc_path, first_virgule_symbol + 1, file_path_length);
 
 	state->fd = open(file_desc_path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 	if (state->fd == -1) {
 		tcmu_err("could not open %s: %m\n", file_desc_path);
 		goto err;
 	}
-	second_split_symbol = strchr(file_desc_path + 1, '/');
-	if (!second_split_symbol) {
+	second_virgule_symbol = strchr(file_desc_path + 1, '/');
+	if (!second_virgule_symbol) {
 		tcmu_err("no configuration found in cfgstring\n");
 		goto err;
 	}
-	length = second_split_symbol - file_desc_path - 1;
+	length = second_virgule_symbol - file_desc_path - 1;
     iqn = (char *) calloc(length, sizeof(char));
     strncpy(iqn, file_desc_path + 1, length);
 	state->iqn = strcat(iqn, "\0");
+    test_demo->fragment_size = strtod(fragment_size_str, &test_demo->fragment_size_unit);
 
 	// Enable the write cache.
  	tcmu_dev_set_write_cache_enabled(dev, 1);
@@ -89,11 +92,14 @@ static int hikvision_open(struct tcmu_device *dev, bool reopen)
 	tcmu_err("config string %s\n", tcmu_dev_get_cfgstring(dev));
 	tcmu_err("iqn of state: %s\n", state->iqn);
 	tcmu_err("fd of state: %d\n", state->fd);
+	tcmu_err("fragment size : %lf\n", state -> fragment_size);
+	tcmu_err("fragment size unit : %s\n", state -> fragment_size_unit);
 
 	hm_private = tcmur_dev_get_private(dev);
 	tcmu_err("iqn of hm_private: %s\n", hm_private->iqn);
 	tcmu_err("fd of state: %d\n", hm_private->fd);
-
+	tcmu_err("fragment size : %lf\n", hm_private -> fragment_size);
+	tcmu_err("fragment size unit : %s\n", hm_private -> fragment_size_unit);
 	return 0;
 
 err:
@@ -129,16 +135,14 @@ static int hikvision_read(struct tcmu_device *dev, struct tcmulib_cmd *cmd,
 		     off_t offset)
 {
 	struct hikvision_state *tcmur_state = tcmur_dev_get_private(dev);
-	struct hikvision_state *tcmu_state = tcmu_dev_get_private(dev);
 	size_t remaining = length;
 	ssize_t ret;
 	
 	char *iqn = tcmur_state->iqn;
-	tcmu_err("write with iqn(tcmur): %s\n", iqn);
-	tcmu_err("write with fd(tcmur): %d\n", tcmur_state->fd);
-	tcmu_err("write with iqn(tcmu): %s\n", tcmu_state->iqn);
-	tcmu_err("write with fd(tcmu): %d\n", tcmu_state->fd);
-
+	tcmu_err("read with iqn(tcmur): %s\n", iqn);
+	tcmu_err("read with fd(tcmur): %d\n", tcmur_state->fd);
+	tcmu_err("read with fragment size : %lf\n", tcmur_state -> fragment_size);
+	tcmu_err("read with fragment size unit : %s\n", tcmur_state -> fragment_size_unit);
 
     // Read the file in loop
 	while (remaining) {
@@ -193,9 +197,8 @@ static int hikvision_write(struct tcmu_device *dev, struct tcmulib_cmd *cmd,
 	char *iqn = tcmur_state->iqn;
 	tcmu_err("write with iqn(tcmur): %s\n", iqn);
 	tcmu_err("write with fd(tcmur): %d\n", tcmur_state->fd);
-	tcmu_err("write with iqn(tcmu): %s\n", tcmu_state->iqn);
-	tcmu_err("write with fd(tcmu): %d\n", tcmu_state->fd);
-
+	tcmu_err("write with fragment size : %lf\n", tcmur_state -> fragment_size);
+	tcmu_err("write with fragment size unit : %s\n", tcmur_state -> fragment_size_unit);
 
     // Write the file in loop
 	while (remaining) {
